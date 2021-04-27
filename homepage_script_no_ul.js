c3c77d9d0f4d10b3258b55625e29310c082e00ab
Jimmy Song <contact@jsong.ca>
2021-04-26 20:00:52 -0400

    add carousel demo

--- homepage_script_no_ul.js
+++ homepage_script_no_ul.js
@@ -1,92 +1,89 @@
-$(document).ready(function() {
-		var FLAG_wobblyness = 20; //range of movement from centre
-		var FLAG_intervalRate = 500;
-		var FLAG_RotRange = 25;
-		var FLAG_SkewRange = 25;
-		var wobblyTextSpans = ".wobblyText span";
+$(document).ready(function () {
+  var FLAG_wobblyness = 20; //range of movement from centre
+  var FLAG_intervalRate = 500;
+  var FLAG_RotRange = 25;
+  var FLAG_SkewRange = 25;
+  var wobblyTextSpans = ".wobblyText span";
 
-		var interval = 0;
-		var eachletter = init();
+  var interval = 0;
+  var eachletter = init();
 
-		/***** startup stuff ******/
+  /***** startup stuff ******/
 
-		function init() {
-			$(".wobblyText").each(function() {
-				console.log("Found Element" + $(this).text());
-				var wobbleHTML;
+  function init() {
+    $(".wobblyText").each(function () {
+      var wobbleHTML;
 
-				if (this.classList && Object.values(this.classList).includes("word")) {
-					wobbleHTML = addSpans(splitStringOnSpaces($(this).text()));
-				} else {
-					wobbleHTML = addSpans(splitString($(this).text()));
-				}
-				replaceWobbleTextHTML(wobbleHTML, this);
-			});
+      if (this.classList && Object.values(this.classList).includes("word")) {
+        wobbleHTML = addSpans(splitStringOnSpaces($(this).text()));
+      } else {
+        wobbleHTML = addSpans(splitString($(this).text()));
+      }
+      replaceWobbleTextHTML(wobbleHTML, this);
+    });
 
-			interval = setInterval(intervalFunction, FLAG_intervalRate);
-		}
+    interval = setInterval(intervalFunction, FLAG_intervalRate);
+  }
 
-		//split all characters in string into array
-		function splitString(strInput) {
-			return strInput.split("");
-		}
+  //split all characters in string into array
+  function splitString(strInput) {
+    return strInput.split("");
+  }
 
-		// split string into array on 'space' character
-		function splitStringOnSpaces(strInput) {
-			return strInput.split(" ");
-		}
+  // split string into array on 'space' character
+  function splitStringOnSpaces(strInput) {
+    return strInput.split(" ");
+  }
 
-		//make a string of each character inside a span
-		function addSpans(arrInput) {
-			var strInputSpans = " ";
+  //make a string of each character inside a span
+  function addSpans(arrInput) {
+    var strInputSpans = " ";
 
-			function addSpan(charInput) {
-				var charToInput = charInput;
+    function addSpan(charInput) {
+      var charToInput = charInput;
 
-				if (charToInput === "") charToInput = "&nbsp";
-				strInputSpans += "<span>" + charToInput + "</span>";
-			}
+      if (charToInput === "") charToInput = "&nbsp";
+      strInputSpans += "<span>" + charToInput + "</span>";
+    }
 
-			arrInput.forEach(addSpan);
+    arrInput.forEach(addSpan);
 
-			return strInputSpans;
-		}
+    return strInputSpans;
+  }
 
-		/***** element HTML & CSS modification ******/
+  /***** element HTML & CSS modification ******/
 
-		function setWobbleCSS($element, top, left, rot, skew) {
-			var tranform = "rotate(" + rot + "deg) skew(" + skew + "deg)";
-			$element.css("top", top);
-			$element.css("left", left);
-			$element.css("transform", tranform);
-		}
+  function setWobbleCSS($element, top, left, rot, skew) {
+    var tranform = "rotate(" + rot + "deg) skew(" + skew + "deg)";
+    $element.css("top", top);
+    $element.css("left", left);
+    $element.css("transform", tranform);
+  }
 
-		function replaceWobbleTextHTML(strInputHTML, inputElement) {
-			$(inputElement).html(strInputHTML);
-		}
+  function replaceWobbleTextHTML(strInputHTML, inputElement) {
+    $(inputElement).html(strInputHTML);
+  }
 
-		/***** maths ******/
+  /***** maths ******/
 
-		//return random number within range min and max
-		function intRandomRange(range) {
-			return Math.random() * (range * 2) - range;
-		}
+  //return random number within range min and max
+  function intRandomRange(range) {
+    return Math.random() * (range * 2) - range;
+  }
 
-		/***** main loop ******/
+  /***** main loop ******/
 
-		function intervalFunction() {
-			$(wobblyTextSpans).each(function(index) {
-				setWobbleCSS(
-					$(this),
-					intRandomRange(FLAG_wobblyness),
-					intRandomRange(FLAG_wobblyness),
-					intRandomRange(FLAG_RotRange),
-					intRandomRange(FLAG_SkewRange)
-				);
-			});
-		}
-	});
+  function intervalFunction() {
+    $(wobblyTextSpans).each(function (index) {
+      setWobbleCSS(
+        $(this),
+        intRandomRange(FLAG_wobblyness),
+        intRandomRange(FLAG_wobblyness),
+        intRandomRange(FLAG_RotRange),
+        intRandomRange(FLAG_SkewRange)
+      );
+    });
+  }
+});
 
-
-	/* image stuff */
-	
+/* image stuff */
